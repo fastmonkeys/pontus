ecedce088885edeeabf610a0854bca672077da0b
Pekka Pöyry <pekka.poyry@fastmonkeys.com>
2018-11-02 19:38:53 +0200

    Allow specifying prefix for new files
    This makes it possible to specify the prefix during validation. In
    some cases we might not know the prefix at the time when the file
    is uploaded, but we know it during the validation.

--- tests/test_amazon_s3_file_validator.py
+++ tests/test_amazon_s3_file_validator.py
@@ -39,6 +39,16 @@ class TestAmazonS3FileValidator(object):
         )
 
     @pytest.fixture
+    def amazon_s3_file_validator_with_prefix(self, bucket):
+        key_name = 'test-unvalidated-uploads/hello.jpg'
+        boto3.resource('s3').Object(bucket.name, key_name).put(Body='test')
+        return AmazonS3FileValidator(
+            key_name=key_name,
+            bucket=bucket,
+            new_file_prefix='validated-uploads/'
+        )
+
+    @pytest.fixture
     def failing_amazon_s3_file_validator(self, bucket):
         key_name = 'images/fail.jpg'
         boto3.resource('s3').Object(bucket.name, key_name).put(Body='test')
@@ -124,6 +134,20 @@ class TestAmazonS3FileValidator(object):
 
         assert boto3.resource('s3').Object(
               bucket.name, 'images/hello.jpg'
+        ).get()
+
+    def test_validate_uses_new_file_prefix(
+        self,
+        amazon_s3_file_validator_with_prefix,
+        bucket
+    ):
+        amazon_s3_file_validator_with_prefix.validate()
+
+        assert amazon_s3_file_validator_with_prefix.obj.key == (
+            'validated-uploads/hello.jpg'
+        )
+        assert boto3.resource('s3').Object(
+              bucket.name, 'validated-uploads/hello.jpg'
         ).get()
 
     def test_repr(self, amazon_s3_file_validator):
