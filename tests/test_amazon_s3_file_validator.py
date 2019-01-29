77af75d448b621986f8726e2cd754126f1ed5115
Tero Vuotila <tero.vuotila@gmail.com>
2019-01-29 11:51:36 +0200

    Add new_file_acl argument to AmazonS3FileValidator

--- tests/test_amazon_s3_file_validator.py
+++ tests/test_amazon_s3_file_validator.py
@@ -11,6 +11,11 @@ from pontus.validators import BaseValidator
 HOUR_IN_SECONDS = 60 * 60
 
 
+class AnyDict:
+    def __eq__(self, other):
+        return isinstance(other, dict)
+
+
 class CustomValidator(BaseValidator):
     def __call__(self, obj):
         if obj.key != 'test-unvalidated-uploads/images/hello.jpg':
@@ -46,6 +51,17 @@ class TestAmazonS3FileValidator(object):
             key_name=key_name,
             bucket=bucket,
             new_file_prefix='validated-uploads/'
+        )
+
+
+    @pytest.fixture
+    def amazon_s3_file_validator_with_acl(self, bucket):
+        key_name = 'test-unvalidated-uploads/images/hello.jpg'
+        boto3.resource('s3').Object(bucket.name, key_name).put(Body='test')
+        return AmazonS3FileValidator(
+            key_name=key_name,
+            bucket=bucket,
+            new_file_acl='private',
         )
 
     @pytest.fixture
@@ -149,6 +165,20 @@ class TestAmazonS3FileValidator(object):
         assert boto3.resource('s3').Object(
               bucket.name, 'validated-uploads/hello.jpg'
         ).get()
+
+    def test_validate_uses_new_file_acl(
+        self,
+        amazon_s3_file_validator_with_acl,
+        bucket
+    ):
+        amazon_s3_file_validator_with_acl.validate()
+
+        assert boto3.resource('s3').Object(
+              bucket.name, 'images/hello.jpg'
+        ).Acl().grants == [{
+            'Grantee': AnyDict(),
+            'Permission': 'FULL_CONTROL'
+        }]
 
     def test_repr(self, amazon_s3_file_validator):
         assert repr(amazon_s3_file_validator) == (
